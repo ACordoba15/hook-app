7ab6d67c667bc6cdb2ae2db1635c2fc5061a65b1
andres <jancordoba05@gmail.com>
2021-06-30 12:04:56 -0600

    .

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -2,33 +2,24 @@ import React, { useState } from 'react';
 import './counter.css';
 
 export const CounterApp = () => {
-
   const [counter, setCounter] = useState(10);
-
   return (
     <>
       <h1>Counter { counter }</h1>
       <hr/>
-
-      <button 
-        className="btn btn-primary"
+      <button className="btn btn-primary"
         onClick={ () => {
           setCounter(counter + 1);
           console.log(counter);
         }}
-      >
-       +1
-      </button>
+      > +1 </button>
       
-      <button 
-        className="btn btn-primary"
+      <button className="btn btn-primary"
         onClick={ () => {
           setCounter(counter - 1);
           console.log(counter);
         }}
-      >
-       -1
-      </button>
+      > -1 </button>
     </>
   )
 }
